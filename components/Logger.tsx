055523e08f0be49064eae9ba764b283079d95bbb
matewilk <matewilk@gmail.com>
2023-01-05 14:34:10 +0000

    (feat): add Winston logs examples to Logger

--- components/Logger.tsx
+++ components/Logger.tsx
@@ -1,7 +1,26 @@
 const winston = require("winston");
 
+/* Example logger with custom formatter
+https://github.com/winstonjs/winston#combining-formats
+const { combine, timestamp, label, prettyPrint } = winston.format;
+const logger = winston.createLogger({
+  format: combine(
+    label({ label: "NextJs NR Integration" }),
+    timestamp(),
+    prettyPrint()
+  ),
+  transports: [new winston.transports.Console()],
+});
+*/
+
+/* Example logger outputing to the console without formatting
 const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
+*/
+
+// thsi will simply send logs to NR without outputting to the console
+// best for prod environments
+const logger = winston.createLogger();
 
 export { logger };
